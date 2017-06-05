f949de0723b856b185fbe32d0da2abd5968e34b0
Carson Cass <cassc6073@wrdsb.ca>
2017-06-05 10:42:00 -0400

    Little scene

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -2,24 +2,22 @@
 
 Scene::Scene(SDL_Window * window) {
   this->window = window;
+}
+
+Scene::~Scene() {
+  SDL_DestoryRenderer(renderer);
+}
+
+void Scene::init() {
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   if(renderer == NULL) {
     std::cout << SDL_GetError();
     SDL_Quit();
   }
-
-}
-
-Scene::~Scene() {
-
-}
-
-void Scene::init() {
-
 }
 
 void Scene::setScene() {
-
+  
 }
 
 void Scene::update() {
@@ -31,5 +29,5 @@ void Scene::addObject() {
 }
 
 void Scene::removeObject() {
-  
+
 }
