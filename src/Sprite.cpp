69703f2b76ceab693ee5b55fe87def86179f6e1f
browr3317 <browr3317@EDS-IC0026503.wrdsb.ca>
2017-06-06 09:33:03 -0400

    Sprite work stuff

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -1,7 +1,26 @@
 #include "Sprite.h"
+ 
 
-Sprite::Sprite() {
+void Sprite::setup(){
+    
+    SDL_Surface* surface = IMG_Load(path.c_str());
+    if(surface = NULL)
+         {
+    printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+  }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if(texture = NULL)
+    {
+     printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+  }
+    Width = surface->w;
+ succ   Height = surface->h;
+    SDL_FreeSurface( surface );
+}
+    
 
+Sprite::Sprite(std::string path, double scale=1, SDL_Renderer* renderer) {
+    this->renderer = renderer;
 }
 
 Sprite::~Sprite() {
@@ -9,5 +28,5 @@ Sprite::~Sprite() {
 }
 
 SDL_Texture * Sprite::getTexture() {
-  
+    return Texture;
 }
--- src/Sprite.h
+++ src/Sprite.h
@@ -5,14 +5,22 @@
 
 #include <string>
 #include "SDL.h"
+#include "Game.h"
 
 class Sprite {
 public:
-  Sprite(std::string path, double scale=1);
+  Sprite(std::string path, double scale=1, SDL_Renderer* renderer);
   virtual ~Sprite();
-  SDL_Texture * getTexture();
+  SDL_Texture* getTexture();
+  void makeTexture(std::string path);
   void setup();
   void render();
+private:
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+    double height;
+    double width;
+    double scale;
 };
 
 #endif
